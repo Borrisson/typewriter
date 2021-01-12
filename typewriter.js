364ee5f8989003a4e4724b0a918306add2d7a397
Borrisson <lucborris@gmail.com>
2021-01-12 14:12:47 +0000

    added comments for future reference

--- typewriter.js
+++ typewriter.js
@@ -1,14 +1,14 @@
 const sentence = "hello there from lighthouse labs";
 let x = 50;
 for (const char of sentence) {
-  if (sentence.length * 50  === x) {
+  if (sentence.length * 50  === x) { // if we are at the end of our sentence stop the process
     setTimeout(() => {
-      process.stdout.write('\n');
-    }, x); // <= 1s delay to make it noticeable. Can dial it down later.
+      process.stdout.write('\n'); 
+    }, x); 
     continue;
   }
   setTimeout(() => {
-    process.stdout.write(char);
+    process.stdout.write(char); // print char on one line.
   }, x); // <= 1s delay to make it noticeable. Can dial it down later.
   x += 50;
 }
